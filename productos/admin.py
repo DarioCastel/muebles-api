4a62151f337ba1fd875f3692a52cc1e8f2d9addb
Dario Castelnovo <lecastelnovo@gmail.com>
2026-02-19 10:19:49 -0300

    fix: producto imagenes como inline en admin

--- productos/admin.py
+++ productos/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
-from .models import Categoria, Producto, ProductoImagen, Combo
+from .models import Producto, ProductoImagen, Categoria, Combo
+
+
+class ProductoImagenInline(admin.TabularInline):
+    model = ProductoImagen
+    extra = 1
+
+
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    inlines = [ProductoImagenInline]
+    list_display = ("nombre", "categoria", "precio", "stock")
+    prepopulated_fields = {"slug": ("nombre",)}
+
 
 admin.site.register(Categoria)
-admin.site.register(Producto)
-admin.site.register(ProductoImagen)
 admin.site.register(Combo)
